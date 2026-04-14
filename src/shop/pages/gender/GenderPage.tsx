14c56d670dbc52b9db820c519f76c00588fd3cc3
user <gterrazas@tecnologiza.me>
2026-04-13 23:44:46 -0600

    Panel administrativo de productos

--- src/shop/pages/gender/GenderPage.tsx
+++ src/shop/pages/gender/GenderPage.tsx
@@ -1,7 +1,23 @@
+import { CustomPagination } from "@/components/custom/CustomPagination"
+import { products } from "@/mocks/productos.mock"
+import { CustomJumbotron } from "@/shop/components/CustomJumbotron"
+import { ProductsGrid } from "@/shop/components/ProductsGrid"
+import { useParams } from "react-router"
 
 
 export const GenderPage = () => {
+
+    const { gender } = useParams();
+
+    const genderLabel = gender == 'men' ? 'Hombres' : gender == 'women' ? 'Mujeres' : 'Niños';
+
     return (
-        <div>GenderPage</div>
+        <>
+            <CustomJumbotron title={`Productos para ${genderLabel}`} />
+
+            <ProductsGrid products={products} />
+
+            <CustomPagination totalPages={5} />
+        </>
     )
 }
